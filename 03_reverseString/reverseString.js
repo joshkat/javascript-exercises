7a017101f9f606c52cc376f3497ffe3d9e4ad079
joshkat <joshua02k@gmail.com>
2022-05-19 21:53:09 -0400

    reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let char1 = string.charAt(0);
+    let newString = '';
+    for(j=string.length; j>=0; j--){
+        newString = newString + string.charAt(j);
+    }
+    return newString;
 };
 
 // Do not edit below this line
