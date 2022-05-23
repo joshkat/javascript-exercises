879c0648783fc3450e62d4efe3c56d4d4fcccbcb
joshkat <joshua02k@gmail.com>
2022-05-23 17:02:47 -0400

    odin

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, removeMe) {
+    for(i=0; i<array.length; i++){
+       if(array[i] == removeMe)    array.splice(i,1);
+    }
+    return array;
 };
 
 // Do not edit below this line
