81668e425f87706279a9c547259e6baa23198ddc
joshkat <joshua02k@gmail.com>
2022-08-03 23:38:40 -0400

    fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if(number < 0)    return 'OOPS';
+    if(number == 0)   return 0;
+    if(number == 1)   return 1;
+    return fibonacci(number-1) + fibonacci(number-2);
 };
+
+console.log(fibonacci(100));
 
 // Do not edit below this line
 module.exports = fibonacci;
