fdb7a785905ad1805a8c2ee143b62dd12ebb02ee
joshkat <joshua02k@gmail.com>
2022-08-03 23:46:10 -0400

    getTheTitles complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    var returnArray = new Array();
+    for(i=0; i<books.length; i++){
+        returnArray[i] = books[i].title;
+    }
+    return returnArray;
 };
 
 // Do not edit below this line
