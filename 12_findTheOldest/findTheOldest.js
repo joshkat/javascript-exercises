84bf94fae872852a06fd0c2f71350ed1f73e84ac
joshkat <joshua02k@gmail.com>
2022-08-04 18:34:39 -0400

    find oldest done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,48 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    var oldestPerson = people[0];
+    for(i=0; i<people.length; i++){
+        if(getAge(oldestPerson) < getAge(people[i]))    {
+            oldestPerson = people[i];
+        }
+    }
+    return oldestPerson;
 };
+
+function getAge(person){
+    var age = 0;
+    let currentYear = new Date().getFullYear();
+    if('yearOfDeath' in person && 'yearOfBirth' in person){
+        age = person.yearOfDeath - person.yearOfBirth;
+        return age;
+    }else{
+        age = currentYear - person.yearOfBirth;
+        return age;
+    }
+}
+
+//1. run through list and compare each persons age, if person a
+//  > person b keep going if person a < person b replace var w
+// person b
+
+const peoples = [
+    {
+        name: "Carly",
+        yearOfBirth: 2018,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      }
+  ]
+
+console.log(getAge(peoples[1]));
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
