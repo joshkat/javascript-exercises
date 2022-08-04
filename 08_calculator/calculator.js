c75054d65a84ca9dc93bb78e86a37771a4b67c8c
joshkat <joshua02k@gmail.com>
2022-08-03 23:11:46 -0400

    fixed jest and finished calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(numA, numB) {
+  return numA+numB;
 };
 
-const subtract = function() {
-	
+const subtract = function(numA, numB) {
+	return numA - numB;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  var sum = 0;
+	for(i=0; i<numArray.length; i++){
+    sum = sum + numArray[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  var sum = 1;
+	for(i=0; i<numArray.length; i++){
+    sum = sum * numArray[i];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(numA, numB) {
+  var temp = numA;
+	for(i=0; i<numB-1; i++){
+    numA = numA * temp;
+  }
+  return numA;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) { //recursive factorial why not
+	if(num == 0) return 1;
+  return num * factorial(num-1);
 };
 
 // Do not edit below this line
